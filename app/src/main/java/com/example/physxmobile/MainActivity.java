005bf9f98348339aeafb68fcd337023b093b36c1
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2021-11-30 20:25:08 +0700

    Added navigation host and LoginFragment

--- app/src/main/java/com/example/physxmobile/MainActivity.java
+++ app/src/main/java/com/example/physxmobile/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.physxmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.mainBottomNavigationView);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.mainFragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }

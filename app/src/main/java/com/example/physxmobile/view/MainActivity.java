86a046e313195ada31ab071336573f0e8a9ac6b4
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2021-12-04 14:49:32 +0700

    Login Register Fundamentals

--- app/src/main/java/com/example/physxmobile/view/MainActivity.java
+++ app/src/main/java/com/example/physxmobile/view/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.physxmobile;
+package com.example.physxmobile.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -7,6 +7,7 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.example.physxmobile.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {

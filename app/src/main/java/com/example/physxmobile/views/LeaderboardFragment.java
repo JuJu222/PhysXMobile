e42d45fc68d178950e65ef83eac912a04bce852e
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2022-01-03 13:40:28 +0700

    Added Back Button And Leaderboard RV

--- app/src/main/java/com/example/physxmobile/views/LeaderboardFragment.java
+++ app/src/main/java/com/example/physxmobile/views/LeaderboardFragment.java
@@ -3,6 +3,8 @@ package com.example.physxmobile.views;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -61,6 +63,11 @@ public class LeaderboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_leaderboard, container, false);
+        View view = inflater.inflate(R.layout.fragment_leaderboard, container, false);
+
+
+        RecyclerView chooseTopic = view.findViewById(R.id.topic_choose_rv);
+        chooseTopic.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false));
+        return view;
     }
 }
